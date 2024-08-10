c854f89f77b7d076b2b5b70f76090208c0a4ba20
ozantalay <ozntalay@hotmail.com>
2024-08-11 00:28:31 +0300

    problem-08 propslarla dinamik hale getirildi

---
+++ src/App.js
@@ -0,0 +1,45 @@
+// Statik değerleri alın ve prop'ları kullanarak dinamik hale getirin
+// Örneğin: label id, label text, input placeholder, input description
+function FormInput({labelId,labelText,inputPlaceholder,inputDescription}) {
+  return (
+    <div className='max-w-sm p-8'>
+      <label
+        htmlFor={labelId}
+        className='block text-sm font-medium leading-6 text-gray-900'
+      >
+        {labelText}
+      </label>
+      <div className='mt-2'>
+        <input
+          id={labelId}
+          name={labelId}
+          type={inputPlaceholder}
+          placeholder='you@example.com'
+          className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2'
+        />
+      </div>
+      <p className='mt-2 text-sm text-gray-500'>
+        {inputDescription}
+      </p>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <form>
+      <FormInput
+      labelId="email"
+      labelText="username"
+      inputPlaceholder="you@example.com" 
+      inputDescription="Bunu sadece spam için kullanacağız." 
+      />
+      <FormInput 
+       labelId="username" 
+       labelText="Username" 
+       inputPlaceholder="john_doe" 
+       inputDescription="Kullanıcı adınız herkese açık olacaktır." 
+      />
+    </form>
+  )
+}
